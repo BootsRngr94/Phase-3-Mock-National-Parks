acd6faed49c4bf131293e669727c80227141b3b4
Eleanor Kelman <ekelman3@gmail.com>
2023-10-11 10:19:00 -0400

    set up multi-file structure for lecture

---
+++ lib/classes/NationalPark.py
@@ -0,0 +1,16 @@
+class NationalPark:
+
+    def __init__(self, name):
+        self.name = name
+        
+    def trips(self):
+        pass
+    
+    def visitors(self):
+        pass
+    
+    def total_visits(self):
+        pass
+    
+    def best_visitor(self):
+        pass
---
+++ lib/classes/Visitor.py
@@ -0,0 +1,13 @@
+class Visitor:
+
+    def __init__(self, name):
+        self.name = name
+        
+    def trips(self):
+        pass
+    
+    def national_parks(self):
+        pass
+    
+    def total_visits_at_park(self, park):
+        pass
